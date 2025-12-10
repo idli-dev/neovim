4e3995f255c04af0933a6d69850e95d3d566d176
idli-dev <241423031+idli-dev@users.noreply.github.com>
2025-12-10 09:43:49 +0530

    added colorscheme KANGAWA

---
+++ lua/config/options.lua
@@ -0,0 +1,49 @@
+local o = vim.opt
+
+o.number = true
+o.relativenumber = true
+o.signcolumn = "yes"
+
+o.scrolloff = 10
+o.sidescrolloff = 10
+o.smoothscroll = true
+
+o.background = "dark"
+o.termguicolors = true
+
+o.wrap = false
+o.breakindent = false
+
+o.autoindent = true
+o.smartindent = true
+
+o.tabstop = 2
+o.softtabstop = 2
+o.shiftwidth = 2
+o.expandtab = true
+
+o.ignorecase = true
+o.smartcase = true
+
+o.backup = false
+o.swapfile = false
+o.autoread = true
+o.undofile = true
+
+o.showmode = false
+o.cmdheight = 0
+
+o.guicursor = "n-c-v:block-nCursor"
+
+o.foldmethod = "indent"  -- or "expr"
+o.foldlevel = 99
+o.hidden = true
+o.timeoutlen = 500
+o.updatetime = 200
+o.mouse = "a"
+
+o.virtualedit = "block"
+o.inccommand = "split"
+
+o.pumheight = 10
+o.pumwidth = 0
